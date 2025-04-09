20814d0b9b1b0fb14492d5fc3564adc593f299f9
benjaminnnnnnnnnnnnn <167529563+benjaminnnnnnnnnnnnn@users.noreply.github.com>
2025-04-09 10:34:17 +0200

    update for web_app

--- Livre_API/src/app.mjs
+++ Livre_API/src/app.mjs
@@ -1,36 +1,45 @@
 import express from "express";
+import cors from "cors"; // <=== CORS, yes yes preciouss!
 import swaggerUi from "swagger-ui-express";
+
 const app = express();
+
+// ✨ Use CORS before routes
+app.use(cors({
+  origin: 'http://localhost:5173', // Vue app
+  credentials: true
+}));
+
 app.use(express.json());
 const port = 3000;
 
 import { swaggerSpec } from "./swagger.mjs";
-// Route pour accéder à la documentation Swagger
-//const specs = swaggerJsdoc(options);
 app.use(
-"/api-docs",
-swaggerUi.serve,
-swaggerUi.setup(swaggerSpec, { explorer: true })
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerSpec, { explorer: true })
 );
+
 // connect to db
 import { sequelize, initDb } from "./db/sequelize.mjs";
 sequelize
-	.authenticate()
-	.then((_) =>
-		console.log("La connexion à la base de données a bien été établie")
-	)
-	.catch((error) => console.error("Impossible de se connecter à la DB"));
+  .authenticate()
+  .then(() =>
+    console.log("La connexion à la base de données a bien été établie")
+  )
+  .catch((error) => console.error("Impossible de se connecter à la DB"));
 
 initDb();
 
 app.get("/", (req, res) => {
-	res.send("API REST of self service machine !");
+  res.send("API REST of self service machine !");
 });
 
 app.get("/api/", (req, res) => {
-	res.redirect(`http://localhost:${port}/`);
+  res.redirect(`http://localhost:${port}/`);
 });
 
+// routes
 import { OuvragesRouter } from "./routes/Livres.mjs";
 app.use("/api/livres", OuvragesRouter);
 
@@ -43,20 +52,19 @@ app.use("/api/categories", CategoriesRouter);
 import { AppreciationRouter } from "./routes/Appreciation.mjs";
 app.use("/api/appreciation", AppreciationRouter);
 
-
 import { loginRouter } from "./routes/login.mjs";
 app.use("/api/login", loginRouter);
 
 import { AuteursRouter } from "./routes/Auteur.mjs";
 app.use("/api/auteurs", AuteursRouter);
 
-// Si aucune route ne correspondant à l'URL demandée par le consommateur
+// 404 handler
 app.use(({ res }) => {
-	const message =
-		"Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.";
-	res.status(404).json(message);
+  const message =
+    "Impossible de trouver la ressource demandée ! Vous pouvez essayer une autre URL.";
+  res.status(404).json(message);
 });
 
 app.listen(port, () => {
-	console.log(`Example app listening on port http://localhost:${port}`);
+  console.log(`Example app listening on port http://localhost:${port}`);
 });
--- Livre_API/src/db/mock-livre.mjs
+++ Livre_API/src/db/mock-livre.mjs
@@ -1,8 +1,21 @@
 let livres = [
 	{
 		idOurage: 1,
-		titre: "Livre 1",
+		titre: "Harry poter",
 		nbPages: 5,
+		extrait: "extrait",
+		resume: "harry poter est un livre écrit par J.K rolling",
+		anneeEdition: 2021,
+		moyenneAppreciation: 5,
+		imageCouverture: "imageCouverture",
+		idCategorie: 1,
+		idAuteur: 1,
+		idEditeur: 1,
+	},
+	{
+		idOurage: 2,
+		titre: "hunger games",
+		nbPages: 45,
 		extrait: "extrait",
 		resume: "resume",
 		anneeEdition: 2021,
